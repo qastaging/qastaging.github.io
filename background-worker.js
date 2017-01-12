1945121eaf402d57b629e9679c3eee3250210ef1
Kedar Tiwaskar <kedar.tiwaskar@gmail.com>
2017-01-12 19:13:15 +0530

    Fixes

---
+++ background-worker.js
@@ -0,0 +1,2 @@
+var version = 2;
+importScripts('https://cdn.pushcrewstaging.com/sw/27e740a2b485338a877d615ca89cf4ec.js');
---
+++ ffsw-pushcrew.js
@@ -0,0 +1,2 @@
+var version = 2;
+importScripts('https://cdn.pushcrewstaging.com/sw/ffsw-27e740a2b485338a877d615ca89cf4ec.js');
