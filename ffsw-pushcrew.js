a355c9770850a6c77ffdbec0f2621c2260ab5dfa
Kedar Tiwaskar <kedar.tiwaskar@gmail.com>
2017-01-20 16:15:35 +0530

    Fixes

--- ffsw-pushcrew.js
+++ ffsw-pushcrew.js
@@ -1,2 +1,2 @@
 var version = 2;
-importScripts('https://cdn.pushcrew.com/sw/ffsw-0502782cfd706f1fe22ff7f4392fd645.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/ffsw-0502782cfd706f1fe22ff7f4392fd645.js');
