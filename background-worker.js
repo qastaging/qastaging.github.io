e30dd817706fa3b93e09d105bb81e2b082db5f94
Kedar Tiwaskar <kedar.tiwaskar@gmail.com>
2017-01-24 16:54:14 +0530

    HTTPS testing

--- background-worker.js
+++ background-worker.js
@@ -1,2 +1,2 @@
 var version = 2;
-importScripts('https://cdn.pushcrew.com/sw/885606ec93de53500f5e75c4bd17415d.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/885606ec93de53500f5e75c4bd17415d.js');
