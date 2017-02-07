7dcf4c7f2070adfcffcdc766a8797f80f0f91bdc
Kedar Tiwaskar <kedar.tiwaskar@gmail.com>
2017-02-07 16:50:19 +0530

    Changes

--- background-worker.js
+++ background-worker.js
@@ -1,2 +1,1 @@
-var version = 2;
-importScripts('https://cdn.pushcrewstaging.com/sw/4acd02fa7be469e91f249e3a5a96cf7b.js');
+importScripts('https://cdn.pushcrew.com/sw/398b988e2da4dc9b17542b09090c3f76.js');
