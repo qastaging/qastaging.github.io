a0a081ba8f28a788ac825e8d7e6b54ee9d58f874
Kedar Tiwaskar <kedar.tiwaskar@gmail.com>
2017-02-13 18:14:51 +0530

    Fixes checking on Prod

--- background-worker.js
+++
@@ -1,2 +0,0 @@
-var version = 2;
-importScripts('https://cdn.pushcrewstaging.com/sw/dfbba173594a2329901a2602eb281ecb.js');
--- ffsw-pushcrew.js
+++
@@ -1,2 +0,0 @@
-var version = 2;
-importScripts('https://cdn.pushcrewstaging.com/sw/ffsw-dfbba173594a2329901a2602eb281ecb.js');
