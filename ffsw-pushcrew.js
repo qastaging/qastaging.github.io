1e98c424cdc4930d489237dd759ab5c8f4002ba5
Kedar Tiwaskar <kedar.tiwaskar@gmail.com>
2017-03-14 19:50:04 +0530

    Test

--- ffsw-pushcrew.js
+++ ffsw-pushcrew.js
@@ -1,2 +1,2 @@
 var version = 2;
-importScripts('https://cdn.pushcrew.com/sw/ffsw-b1799ad6eaf612b6d9ae9963ab9abdb9.js');
+importScripts('https://cdn.pushcrew.com/sw/ffsw-3448ee84821725d7272f203e576fa7e7.js');
