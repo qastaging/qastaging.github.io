7126b885d450d20b5efbc781721dcc0550fd4ef4
Kedar Tiwaskar <kedar.tiwaskar@gmail.com>
2017-03-23 11:47:56 +0530

    Changes

--- background-worker.js
+++ background-worker.js
@@ -1,2 +1,2 @@
 var version = 2;
-importScripts('https://cdn.pushcrew.com/sw/a7857946c2d75218ceac8b3f21ecd7eb.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/a7857946c2d75218ceac8b3f21ecd7eb.js');
--- ffsw-pushcrew.js
+++ ffsw-pushcrew.js
@@ -1,2 +1,2 @@
 var version = 2;
-importScripts('https://cdn.pushcrew.com/sw/ffsw-a7857946c2d75218ceac8b3f21ecd7eb.js');
+importScripts('https://cdn.pushcrewstaing.com/sw/ffsw-a7857946c2d75218ceac8b3f21ecd7eb.js');
