f10ec316b8ae3d4c899d2456d3469c9d0a1fc5a0
Kedar Tiwaskar <kedar.tiwaskar@gmail.com>
2017-03-29 23:49:23 +0530

    Test

--- background-worker.js
+++ background-worker.js
@@ -1,2 +1,2 @@
 var version = 2;
-importScripts('https://cdn.pushcrew.com/sw/f09988457be07cfd2dfac1465b02f116.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/f37818f96e1bcadabf13dae68d146cce.js');
