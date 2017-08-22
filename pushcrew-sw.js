543a1565def99892d10e8a1cb19382636841e532
Kedar Tiwaskar <kedar.tiwaskar@gmail.com>
2017-08-22 19:00:04 +0530

    Testing5

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
-var version = 2;
-importScripts('https://cdn.pushcrewstaging.com/sw/ffsw-d7ae31d54c664103c8e8bd3596326b1b.js');
+var version = 6;
+importScripts('https://cdn.pushcrew.com/sw/b132a04162d64c25bcdb1d8b956603da.js');
