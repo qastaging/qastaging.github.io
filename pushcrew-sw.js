ac904d6aea1aaea6be8dce26994588d9822d1d03
Kedar Tiwaskar <kedar.tiwaskar@gmail.com>
2017-08-22 18:21:42 +0530

    Testing renaming

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 2;
-importScripts('https://cdn.pushcrewstaging.com/sw/d7ae31d54c664103c8e8bd3596326b1b.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/ffsw-d7ae31d54c664103c8e8bd3596326b1b.js');
